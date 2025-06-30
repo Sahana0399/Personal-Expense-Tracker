76c1eb8f6198c9b25979e69702d38177dce9f451
Sahana M R <sahanamadeshwara@gmail.com>
2025-06-30 21:28:09 +0100

    Created:- Signup and login function and storing it in users.csv file

---
+++ main.py
@@ -0,0 +1,72 @@
+import csv
+import pandas as pd
+from datetime import datetime
+
+
+def signup():
+    username = input("Choose a username: ").strip()
+    password = input("Choose a password: ").strip()
+    with open("users.csv",mode= "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([username, password])
+        print("Signup successful!")
+        main()
+
+def login():
+    username = input("Username: ").strip()
+    password = input("Password: ").strip()
+    try:
+        with open("users.csv", mode="r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row == [username, password]:
+                    print("Login successful!")
+                    return username
+        print("Invalid username or password.")
+        main()
+    except FileNotFoundError:
+        print("No users found.")
+        return None
+    
+
+def main():
+    print("Welcome to Expenses Tracker")
+    print("1.Sign Up")
+    print("2.Log In")
+    choice = input("Enter choice (1/2): ").strip()
+    
+    if choice == "1":
+        signup()
+        return
+    elif choice == "2":
+        username = login()
+        if not username:
+            return
+    else:
+        print("Invalid choice.")
+        return
+
+    
+    while True:
+        #print("\n===== Expense Tracker Menu =====")
+        print("\n1.Add Expense")
+        print("2. View Expenses")
+        print("3. Exit")
+
+        action = input("Choose an option : ").strip()
+
+        if action == "1":
+           print(" add_expense(username)")
+        elif action == "2":
+           print(" view_expenses(username)")
+        elif action == "3":
+            print("Logged out.")
+            break
+        else:
+            print("Invalid option.")
+
+
+if __name__ == "__main__":
+    main()
+
+
