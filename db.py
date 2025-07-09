5f5834e17be9bfa4eaf8d76d468c2a0e909eec10
Sahana M R <sahanamadeshwara@gmail.com>
2025-07-09 02:48:39 +0100

    Removed .csv file and pushed data in expenses_tracker.db using sqlite3

---
+++ db.py
@@ -0,0 +1,78 @@
+import sqlite3
+
+def connect():
+    
+    conn = sqlite3.connect("expenses_tracker.db")
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS users(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       username TEXT UNIQUE NOT NULL,
+                       password TEXT NOT NULL
+                    )
+                   
+     ''')
+    conn.commit()
+    conn.close()
+    
+def insert_user(username, password):
+    conn = sqlite3.connect('expenses_tracker.db')
+    cursor = conn.cursor()
+
+    # Insert user data
+    cursor.execute('''
+    INSERT INTO users (username, password)
+    VALUES (?, ?)
+    ''', (username, password))
+
+    conn.commit()  # Save the changes
+    conn.close()
+    
+def insert_expense(username, date, category, amount, location):
+    conn = sqlite3.connect("expenses_tracker.db")
+    cursor = conn.cursor()
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS expense (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       username TEXT,
+                       date TEXT,
+                       category TEXT,
+                       amount REAL,
+                       location TEXT
+                   )
+                   ''')
+    cursor.execute('''
+    INSERT INTO expense(username, date, category, amount, location)               
+    VALUES (?,?,?,?,?)
+    ''', (username,date,category,amount,location))
+    conn.commit()
+    conn.close()
+
+def fetch_user(username,password):
+    conn = sqlite3.connect("expenses_tracker.db")
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+                   SELECT * FROM users WHERE username = ? AND password = ?
+                   ''',( username, password))
+    
+    user = cursor.fetchone()
+    conn.close()
+    return user
+    
+    
+def fetch_expense(username):
+    conn = sqlite3.connect("expenses_tracker.db")
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+                   SELECT date, category,amount, location
+                   FROM expense
+                   WHERE username = ?
+                   ''',( username,))
+    
+    expenses = cursor.fetchall()
+    conn.close()
+    return expenses
+
--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import csv
 import pandas as pd
+from db import connect
+from db import insert_user, fetch_expense , insert_expense,fetch_user
+# from signup_login import signup, login, add_expense, view_expenses
 from datetime import datetime
 
 
 def signup():
     username = input("Choose a username: ").strip()
     password = input("Choose a password: ").strip()
-    with open("users.csv",mode= "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([username, password])
-        print("Signup successful!")
-        main()
-
+    
+    insert_user(username,password)
+    print("Signup successfull!")
+    login()
+    
 def login():
     username = input("Username: ").strip()
     password = input("Password: ").strip()
-    try:
-        with open("users.csv", mode="r") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if row == [username, password]:
-                    print("Login successful!")
-                    return username
+    
+    user = fetch_user(username,password)
+    
+    
+    if user:
+        print("Login Successfull!")
+        return username
+    else:
         print("Invalid username or password.")
-        main()
-    except FileNotFoundError:
-        print("No users found.")
         return None
     
 def add_expense(username):
@@ -33,15 +33,19 @@ def add_expense(username):
     category = input("Enter category (e.g. Food, Transport): ").strip()
     amount = input("Enter amount: ").strip()
     location = input("Enter location/place of expense: ").strip()
+   
     try:
         expense_date = datetime.strptime(date, "%Y-%m-%d")
         today = datetime.today()
 
         if expense_date > today:
-            print("❌ Future dates are not allowed.")
+            print("Future dates are not allowed.")
             return
         
         amount = float(amount)
+        print(username, date, category, amount, location)
+        insert_expense(username, date, category, amount, location)
+        
     except ValueError:
         print("Invalid date or amount format.")
         return
@@ -51,44 +55,39 @@ def add_expense(username):
         writer.writerow([username,date, category, amount, location])
         print("Expense added successfully!")
 
-def view_expenses(username):
-    try:
-        df = pd.read_csv("data.csv", names=["Username","Date", "Category", "Amount","Location"])
-        user_df = df[df["Username"]== username]
-        #df["Amount"] = pd.to_numeric(df["Amount"], errors="coerce")
-        
-        if user_df.empty:
-            print("NO expenses found.")
-            return    
-        #summary = df.groupby(["Date", "Category", "location"])["Amount"].sum().reset_index()
-    
-        print("\n Your Expenses:")
-        for _, row in user_df.iterrows():
-                print(f"{row['Date']} | {row['Category']} |{row['Amount']} |{row['Location']}")
-    except FileNotFoundError:
-        print("No expense found.")
 
+
+def view_expense(username):
+    expenses = fetch_expense(username) # type: ignore
+    if not expenses:
+        print("No records found.")
+        return
+    
+    print("\nDate\t\tCategory\tAmount\tLocation")
+    print("-"*50)
+    for row in expenses:
+        print(f"{row[0]}\t{row[1]}\t{row[2]}\t{row[3]}")
         
 def main():
+    connect()
+
     print("Welcome to Expenses Tracker")
     print("1.Sign Up")
     print("2.Log In")
     choice = input("Enter choice (1/2): ").strip()
     
     if choice == "1":
-        signup()
-        return
+        username = signup()
     elif choice == "2":
         username = login()
-        if not username:
-            return
     else:
         print("Invalid choice.")
         return
-
     
+    if not username:
+        return
+
     while True:
-        #print("\n===== Expense Tracker Menu =====")
         print("\n1.Add Expense")
         print("2. View Expenses")
         print("3. Exit")
@@ -98,7 +97,7 @@ def main():
         if action == "1":
            add_expense(username)
         elif action == "2":
-           view_expenses(username)
+           view_expense(username)
         elif action == "3":
             print("Logged out.")
             break
@@ -108,5 +107,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
